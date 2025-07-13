46e88675879097484fcbdae754311936b4f4dfcc
wusel <wusel+src@uu.org>
2025-07-13 18:59:25 +0200

    Move to v2023.1.2 as 1.9.0

--- site.mk
+++ site.mk
@@ -40,7 +40,7 @@ GLUON_FEATURES_standard := wireless-encryption-wpa3
 #    mtr-json \
 #    ip-full
 
-DEFAULT_GLUON_RELEASE := 1.6.0
+DEFAULT_GLUON_RELEASE := 1.9.0
 
 # Allow overriding the release number from the command line
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
